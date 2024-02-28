c55daf7e193103123599699b29efff3b3f8df65c
lukedee-b <N00220386@iadt.ie>
2024-02-28 04:56:03 +0000

    added comments

--- BarChart.js
+++ BarChart.js
@@ -1,104 +1,115 @@
-class BarChart{
-    constructor(obj){
-        this.data = obj.data;
-        this.yValue = obj.yValue;
-        this.xValue = obj.xValue;
-        this.chartWidth = obj.chartWidth;
-        this.chartHeight = obj.chartHeight;
-        this.xPos = obj.xPos;
-        this.yPos = obj.yPos;
-        this.axisLineColour = obj.axisLineColour;
-        this.labelTextSize = obj.labelTextSize;
-        this.labelPadding = obj.labelPadding;
-        this.labelColour = obj.labelColour;
-        this.labelRotation = obj.labelRotation;
-        this.barWidth = obj.barWidth;
-        this.title = obj.title
-        this.yAxisTitle = obj.yAxisTitle;
-        this.xAxisTitle = obj.xAxisTitle;
+class BarChart {
+  constructor(obj) {
+    this.data = obj.data;
+    this.yValue = obj.yValue;
+    this.xValue = obj.xValue;
+    this.chartWidth = obj.chartWidth;
+    this.chartHeight = obj.chartHeight;
+    this.xPos = obj.xPos;
+    this.yPos = obj.yPos;
+    this.axisLineColour = obj.axisLineColour;
+    this.labelTextSize = obj.labelTextSize;
+    this.labelPadding = obj.labelPadding;
+    this.labelColour = obj.labelColour;
+    this.labelRotation = obj.labelRotation;
+    this.barWidth = obj.barWidth;
+    this.title = obj.title;
+    this.yAxisTitle = obj.yAxisTitle;
+    this.xAxisTitle = obj.xAxisTitle;
+  }
 
+  render() {
+    push();
+
+    // rotate(89.525)
+    // translate(100,-1500)
+
+    // Chart Title
+    textAlign(CENTER);
+    textSize(this.titleTextSize);
+    fill(this.labelColour);
+    text(
+      this.title,
+      this.chartWidth / 2 + this.xPos,
+      this.yPos - this.yPos * 0.9
+    );
+
+    // xAxisTitle
+    textAlign(CENTER);
+    textSize(this.labelTextSize);
+    fill(this.labelColour);
+    text(
+      this.xAxisTitle,
+      this.chartWidth / 2 + this.xPos,
+      this.yPos + this.yPos * 0.3
+    );
+
+    // yAxisTitle
+    textAlign(CENTER);
+    textSize(this.labelTextSize);
+    fill(this.labelColour);
+    text(
+      this.yAxisTitle,
+      this.xPos - 500 + this.yPos,
+      this.xPos + this.yPos * 0.2
+    );
+
+    translate(this.xPos, this.yPos); // sets up where 0,0 is
+    stroke(this.axisLineColour);
+    line(0, 0, 0, -this.chartHeight);
+    line(0, 0, this.chartWidth, 0);
+
+    let gap =
+      (this.chartWidth - this.data.length * this.barWidth) /
+      (this.data.length + 1); // sets up the gaps
+    let labels = this.data.map((d) => d[this.xValue]); // maps out the data
+    let scale = this.chartHeight / max(this.data.map((d) => d[this.yValue])); // scales the height
+    // console.log(scale);
+
+    //this loop draws the horizontal elements bars and labels
+    push();
+    translate(gap, 0);
+    for (let i = 0; i < this.data.length; i++) {
+      fill("#fc4503");
+      noStroke();
+      rect(0, 0, this.barWidth, -this.data[i][this.yValue] * scale);
+
+      // textSize(20);
+      fill(this.labelColour);
+      noStroke();
+      textSize(this.labelTextSize);
+      textAlign(LEFT, CENTER);
+      push();
+      translate(this.barWidth / 2, this.labelPadding);
+      rotate(this.labelRotation);
+
+      text(labels[i], 0, 0);
+      pop();
+
+      translate(gap + this.barWidth, 0);
+    }
+    pop();
+
+    //this draws the vertical elements
+    let tickGap = this.chartHeight / 5; // splits the y axis into 5 different sections
+    let tickValue = max(this.data.map((d) => d[this.yValue])) / 5;
+    for (let i = 0; i <= 5; i++) {
+      stroke("fff");
+      line(0, -i * tickGap, -20, -i * tickGap);
+
+      fill(this.labelColour);
+      noStroke();
+      textSize(this.labelTextSize);
+      textAlign(RIGHT, CENTER);
+      text(tickValue * i, -20, -i * tickGap);
     }
 
-    render(){
-        push ();
-
-        // rotate(89.525)
-        // translate(100,-1500)
-
-        // Chart Title
-        textAlign(CENTER);
-        textSize(this.titleTextSize);
-        fill(this.labelColour);
-        text(this.title, (this.chartWidth/2)+this.xPos, this.yPos-(this.yPos*0.87));
-
-        // xAxisTitle
-        textAlign(CENTER);
-        textSize(this.labelTextSize);
-        fill(this.labelColour);
-        text(this.xAxisTitle, (this.chartWidth/2)+this.xPos, this.yPos+(this.yPos*0.3));
-        
-        // yAxisTitle
-        textAlign(CENTER);
-        textSize(this.labelTextSize);
-        fill(this.labelColour);
-        text(this.yAxisTitle, (this.xPos-500)+this.yPos, this.xPos+(this.yPos*0.2));
-        
-
-        translate (this.xPos,this.yPos);  // sets up where 0,0 is 
-        stroke(this.axisLineColour);
-        line (0,0,0,-this.chartHeight);
-        line (0,0,this.chartWidth,0);
-
-        let gap = (this.chartWidth - (this.data.length * this.barWidth))/(this.data.length + 1) // sets up the gaps
-        let labels = this.data.map(d => d[this.xValue]); // maps out the data
-        let scale = this.chartHeight / max(this.data.map(d=> d[this.yValue])); // scales the height
-        // console.log(scale);
-
-        //this loop draws the horizontal elements bars and labels
-        push()
-        translate(gap,0);
-        for(let i=0; i<this.data.length; i++){
-            fill ("#fc4503");
-            noStroke()
-            rect (0,0,this.barWidth, -this.data[i][this.yValue] * scale);
-
-            // textSize(20);
-            fill(this.labelColour)
-            noStroke()
-            textSize(this.labelTextSize)
-            textAlign(LEFT, CENTER)
-            push()
-            translate(this.barWidth/2,this.labelPadding)
-            rotate(this.labelRotation)
-
-            text (labels[i],0,0);
-            pop()
-            
-            translate(gap+this.barWidth,0);
-        }
-        pop()
-
-        //this draws the vertical elements 
-        let tickGap = this.chartHeight / 5; // splits the y axis into 5 different sections
-        let tickValue = max(this.data.map(d=> d[this.yValue]))/5
-        for(let i=0; i<=5; i++){
-            stroke("fff")
-            line(0,-i*tickGap,-20,-i*tickGap)
-
-            fill(this.labelColour)
-            noStroke()
-            textSize(this.labelTextSize)
-            textAlign(RIGHT, CENTER)
-            text(tickValue*i,-20,-i*tickGap)
-        }
-
-        pop ();
-    }
+    pop();
+  }
 }
 
-
-class HorizontalBarChart{
-constructor(obj) {
+class HorizontalBarChart {
+  constructor(obj) {
     this.data = obj.data;
     this.xValue = obj.xValue;
     this.yValue = obj.yValue;
@@ -118,61 +129,64 @@ constructor(obj) {
     this.TitleSize = obj.TitleSize;
     this.xyTitleSize = obj.xyTitleSize;
   }
- 
- 
+
   render() {
     push();
     translate(this.xPos, this.yPos);
     stroke(this.axisLineColour);
- 
+
     line(0, 0, this.chartWidth, 0);
     line(0, 0, 0, -this.chartHeight);
- 
+
     let gap =
-      ((this.chartWidth - this.data.length * this.barWidth) /this.data.length) * 1.8;
-   
+      ((this.chartWidth - this.data.length * this.barWidth) /
+        this.data.length) *
+      1.8;
+
     let labels = this.data.map((d) => d[this.xValue]);
     let scale = this.chartWidth / max(this.data.map((d) => d[this.yValue]));
- 
+
     fill(this.labelColour);
     textSize(this.TitleSize);
- 
+
     //title
     textAlign(CENTER, TOP);
     noStroke();
     textStyle(NORMAL);
     text(this.title, this.chartWidth / 2, -this.chartHeight - 70);
- 
-//    x title
+
+    //    x title
     fill(this.labelColour);
     textSize(this.xyTitleSize);
     noStroke();
     textAlign(CENTER, TOP);
     text(this.xAxisTitle, this.chartWidth / 2, 90);
 
-// y title
+    // y title
     push();
-    translate(-140, -this.chartHeight /3);
+    translate(-140, -this.chartHeight / 3);
     text(this.yAxisTitle, 0, -90);
     pop();
- 
+
+    // draws the bars
     push();
     translate(1, -gap * 30);
     for (let i = 0; i < this.data.length; i++) {
       fill("#fc4503");
       noStroke();
-      rect(0, i * gap, this.data[i][this.yValue] * scale, this.barWidth); // draws the bar
- 
+      rect(0, i * gap, this.data[i][this.yValue] * scale, this.barWidth);
+
+      // y axis labels
       fill(this.labelColour);
       textSize(this.labelTextSize);
       textAlign(RIGHT, CENTER);
       text(labels[i], -10, i * gap + this.barWidth / 2);
     }
     pop();
- 
+
     let tickGap = this.chartWidth / 5;
     let tickValue = max(this.data.map((d) => d[this.yValue])) / 5;
- 
+
     // labels the x axis
     for (let i = 0; i <= 5; i++) {
       stroke(255);
@@ -187,110 +201,126 @@ constructor(obj) {
   }
 }
 
-
 class StackedBarChart {
-    constructor(obj) {
-        this.data = obj.data;
-        this.yValues = obj.yValues; // Array of values to stack
-        this.xValue = obj.xValue;
-        this.chartWidth = obj.chartWidth;
-        this.chartHeight = obj.chartHeight;
-        this.xPos = obj.xPos;
-        this.yPos = obj.yPos;
-        this.axisLineColour = obj.axisLineColour;
-        this.labelTextSize = obj.labelTextSize;
-        this.labelPadding = obj.labelPadding;
-        this.labelColour = obj.labelColour;
-        this.labelRotation = obj.labelRotation;
-        this.barWidth = obj.barWidth;
-        this.title = obj.title;
-        this.yAxisTitle = obj.yAxisTitle;
-        this.xAxisTitle = obj.xAxisTitle;
-        this.colours = obj.colours
+  constructor(obj) {
+    this.data = obj.data;
+    this.yValues = obj.yValues; // Array of values to stack
+    this.xValue = obj.xValue;
+    this.chartWidth = obj.chartWidth;
+    this.chartHeight = obj.chartHeight;
+    this.xPos = obj.xPos;
+    this.yPos = obj.yPos;
+    this.axisLineColour = obj.axisLineColour;
+    this.labelTextSize = obj.labelTextSize;
+    this.labelPadding = obj.labelPadding;
+    this.labelColour = obj.labelColour;
+    this.labelRotation = obj.labelRotation;
+    this.barWidth = obj.barWidth;
+    this.title = obj.title;
+    this.yAxisTitle = obj.yAxisTitle;
+    this.xAxisTitle = obj.xAxisTitle;
+    this.colours = obj.colours;
+  }
+
+  render() {
+    push();
+
+    // Chart Title
+    textAlign(CENTER);
+    textSize(this.titleSize);
+    fill(this.labelColour);
+    text(
+      this.title,
+      this.xPos + this.chartWidth / 2,
+      this.yPos - this.chartHeight * 1.1
+    ); // positions the chart title
+
+    // xAxisTitle
+    textAlign(CENTER);
+    textSize(this.labelTextSize);
+    fill(this.labelColour);
+    text(
+      this.xAxisTitle,
+      this.xPos + this.chartWidth / 2,
+      this.yPos + this.chartHeight * 0.2
+    ); // positions the x axis title
+
+    // yAxisTitle
+    textAlign(CENTER);
+    textSize(this.labelTextSize);
+    fill(this.labelColour);
+    text(this.yAxisTitle, this.xPos - 100, this.yPos - this.chartHeight / 2); // positions the y axis title
+
+    // this sets up the graph lines
+    translate(this.xPos, this.yPos);
+    stroke(this.axisLineColour);
+    line(0, 0, 0, -this.chartHeight);
+    line(0, 0, this.chartWidth, 0);
+
+    let gap =
+      (this.chartWidth - this.data.length * this.barWidth) /
+      (this.data.length + 1); // decides how big and where to put the gaps
+    let labels = this.data.map((d) => d[this.xValue]);
+
+    // Calculate the maximum value for scaling
+    let maxSum = 0; //Max sum is supposed to be the maximum amount that male and female can combine to which should be 4648
+    for (let i = 0; i < this.data.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < this.yValues.length; j++) {
+        sum += this.data[i][this.yValues[j]];
+      }
+      maxSum = max(maxSum, sum);
+      console.log("sum", sum);
+    }
+    let scale =
+      this.chartHeight /
+      (max(this.data.map((d) => d[this.yValues])) / this.data.length); // is supposed to scale the chart
+    console.log("Max Sum:", maxSum);
+    console.log("Scale:", scale);
+
+    // Draw stacked bars
+    for (let i = 0; i < this.data.length; i++) {
+      // i refers to the Male deaths
+      let y = 0; // Starting point for each stacked bar
+      for (let j = 0; j < this.yValues.length; j++) {
+        // j refers to the Female
+        let value = this.data[i][this.yValues[j]];
+        let barHeight = value * scale;
+        fill(this.colours[1]); // this calls the second colour
+
+        console.log("Drawing rectangle at:",
+          gap + i * (this.barWidth + gap),0,this.barWidth,-barHeight
+        ); // shows us where each bar is being placed
+        rect(0, 0, this.barWidth, -this.data[i][this.yValues]);
+        y += barHeight;
+      }
+
+      // x axis Label
+      fill(this.labelColour);
+      noStroke();
+      textSize(this.labelTextSize);
+      textAlign(LEFT, CENTER);
+      text(
+        labels[i],
+        gap + i * (this.barWidth + gap) + this.barWidth / 2,
+        this.chartHeight + this.labelPadding - 360
+      );
     }
 
-    render() {
-        push();
-    
-        // Chart Title
-        textAlign(CENTER);
-        textSize(this.titleSize);
-        fill(this.labelColour);
-        text(this.title, this.xPos + this.chartWidth / 2, this.yPos - this.chartHeight *1.1); // positions the chart title
-    
-        // xAxisTitle
-        textAlign(CENTER);
-        textSize(this.labelTextSize);
-        fill(this.labelColour);
-        text(this.xAxisTitle, this.xPos + this.chartWidth / 2, this.yPos + this.chartHeight * 0.25); // positions the x axis title
-    
-        // yAxisTitle
-        textAlign(CENTER);
-        textSize(this.labelTextSize);
-        fill(this.labelColour);
-        text(this.yAxisTitle, this.xPos - 100, this.yPos - this.chartHeight / 2); // positions the y axis title
-    
-        // this sets up the graph lines
-        translate(this.xPos, this.yPos);
-        stroke(this.axisLineColour);
-        line(0, 0, 0, -this.chartHeight);
-        line(0, 0, this.chartWidth, 0);
-    
-        let gap = (this.chartWidth - (this.data.length * this.barWidth)) / (this.data.length + 1); // decides how big and where to put the gaps
-        let labels = this.data.map(d => d[this.xValue]);
-    
+    // Draw y-axis ticks
+    let tickGap = this.chartHeight / 5;
+    let tickValue = max(this.data.map((d) => d[this.yValues])) / 5; // Calculate tick value based on maxSum
+    for (let i = 0; i <= 5; i++) {
+      stroke(this.axisLineColour);
+      line(0, -i * tickGap, -20, -i * tickGap);
 
-        
-        // Calculate the maximum value for scaling
-        let maxSum = 0; //Max sum is supposed to be the maximum amount that male and female can combine to which should be 4648
-        for (let i = 0; i < this.data.length; i++) {
-            let sum = 0;
-            for (let j = 0; j < this.yValues.length; j++) {
-                sum += this.data[i][this.yValues[j]];
-            }
-            maxSum = max(maxSum, sum);
-            console.log('sum', sum);
-
-        }
-        let scale = this.chartHeight / (max(this.data.map(d=> d[this.yValues]))/this.data.length); // is supposed to scale the chart 
-        console.log('Max Sum:', maxSum); 
-        console.log('Scale:', scale);
-    
-        // Draw stacked bars
-        for (let i = 0; i < this.data.length; i++) { // i refers to the Male deaths
-            let y = 0; // Starting point for each stacked bar
-            for (let j = 0; j < this.yValues.length; j++) { // j refers to the Female
-                let value = this.data[i][this.yValues[j]];
-                let barHeight = value * scale;
-                fill(this.colours[1]); // this calls the second colour
-
-                console.log('Drawing rectangle at:', gap + i * (this.barWidth + gap), 0, this.barWidth, -barHeight); // shows us where each bar is being placed
-                rect(0, 0, this.barWidth, -(this.data[i][this.yValues]));
-                y += barHeight;
-            }
-    
-            // x axis Label
-            fill(this.labelColour);
-            noStroke();
-            textSize(this.labelTextSize);
-            textAlign(LEFT, CENTER);
-            text(labels[i], (gap + i * (this.barWidth + gap)) + this.barWidth / 2, this.chartHeight + this.labelPadding -360);
-        }
-    
-        // Draw y-axis ticks
-        let tickGap = this.chartHeight / 5;
-        let tickValue = max(this.data.map(d=> d[this.yValues])) / 5; // Calculate tick value based on maxSum
-        for (let i = 0; i <= 5; i++) {
-            stroke(this.axisLineColour);
-            line(0, -i * tickGap, -20, -i * tickGap);
-    
-            fill(this.labelColour);
-            noStroke();
-            textSize(this.labelTextSize);
-            textAlign(RIGHT, CENTER);
-            text(tickValue * i, -20, -i * tickGap);
-        }
-    
-        pop();
+      fill(this.labelColour);
+      noStroke();
+      textSize(this.labelTextSize);
+      textAlign(RIGHT, CENTER);
+      text(tickValue * i, -20, -i * tickGap);
     }
-}    
+
+    pop();
+  }
+}
--- sketch.js
+++ sketch.js
@@ -2,122 +2,180 @@ let barCharts = [];
 let data;
 let data2;
 let data3;
-let cleanData=[];
-let cleanData2=[];
-let cleanData3=[];
+let data4;
+let data5;
+let cleanData = [];
+let cleanData2 = [];
+let cleanData3 = [];
+let cleanData4 = [];
+let cleanData5 = [];
 let numRows;
 let numRows2;
 let numRows3;
+let numRows4;
+let numRows5;
 
-function preload(){
-    data = loadTable("data/RIP.csv", "csv", "header");
-    data2 = loadTable("data/Deaths.csv", "csv", "header");
-    data3 = loadTable("data/MF.csv", "csv", "header");
-
+function preload() {
+  data = loadTable("data/RIP.csv", "csv", "header");
+  data2 = loadTable("data/Deaths.csv", "csv", "header");
+  data3 = loadTable("data/MF.csv", "csv", "header");
+  data4 = loadTable("data/MDeaths.csv", "csv", "header");
+  data5 = loadTable("data/FDeaths.csv", "csv", "header");
 }
 
-function setup(){
-    noLoop()
-    background(50)
-    createCanvas(3000,7000)
-
-    numRows = data.rows.length;
-    for(let i=0; i<numRows; i++){
-        cleanData.push(data.rows[i].obj)
-    }
+function setup() {
+  noLoop();
+  background(50);
+  createCanvas(3000, 7000);
 
 
-    let barChart01 = {
-    data:cleanData,
-    yValue:"VALUE",
-    xValue:"Month",
-    chartWidth:500,
-    chartHeight:350,
-    xPos:150,
-    yPos:400,
-    axisLineColour:"#d9d9d9",
+  //normal bar chart
+  numRows = data.rows.length;
+  for (let i = 0; i < numRows; i++) {
+    cleanData.push(data.rows[i].obj);
+  }
+
+  let barChart01 = {
+    data: cleanData,
+    yValue: "VALUE",
+    xValue: "Month",
+    chartWidth: 500,
+    chartHeight: 350,
+    xPos: 150,
+    yPos: 400,
+    axisLineColour: "#d9d9d9",
     barWidth: 10,
-    labelTextSize:12,
-    labelPadding:10,
-    labelColour:"#fc4503",
-    labelRotation:45,
-    title:"Total of Registered Deaths (From 2021-2023)",
-    xAxisTitle:"Year and Month",
-    yAxisTitle:"Death Cases",
-    }
+    labelTextSize: 12,
+    labelPadding: 10,
+    labelColour: "#fc4503",
+    labelRotation: 45,
+    title: "Total of Registered Deaths (From 2021-2023)",
+    xAxisTitle: "Year and Month",
+    yAxisTitle: "Death Cases",
+  };
+
+  barCharts.push(new BarChart(barChart01));
+
+  //second normal bar chart 
+  numRows4 = data4.rows.length;
+  for (let i = 0; i < numRows4; i++) {
+    cleanData4.push(data4.rows[i].obj);
+  }
+
+  let barChart04 = {
+    data: cleanData4,
+    yValue: "VALUE",
+    xValue: "Month",
+    chartWidth: 500,
+    chartHeight: 350,
+    xPos: 900,
+    yPos: 400,
+    axisLineColour: "#d9d9d9",
+    barWidth: 10,
+    labelTextSize: 12,
+    labelPadding: 10,
+    labelColour: "#fc4503",
+    labelRotation: 45,
+    title: "Male Registered Deaths from 2019-2021",
+    xAxisTitle: "Year and Month",
+    yAxisTitle: "",
+  };
+
+  barCharts.push(new BarChart(barChart04));
 
 
-    barCharts.push(new BarChart(barChart01))
+// horizontal bar chart 
+  numRows2 = data.rows.length;
+  for (let i = 0; i < numRows; i++) {
+    cleanData2.push(data.rows[i].obj);
+  }
 
+  let barChart02 = {
+    data: cleanData2,
+    xValue: "Month",
+    yValue: "VALUE",
+    chartWidth: 500,
+    chartHeight: 361,
+    xPos: 1000,
+    yPos: 1100,
+    axisLineColour: "#d9d9d9",
+    barWidth: 10,
+    labelTextSize: 12,
+    labelPadding: 10,
+    labelColour: "#fc4503",
+    labelRotation: -45,
+    title: "This is a test",
+    xAxisTitle: "Year and Month",
+    yAxisTitle: "Death Cases",
+    title: "Total of Registered Deaths (From 2021-2023)",
+    TitleSize: 12,
+    labelTextSize: 12,
+  };
 
-    numRows2 = data.rows.length;
-    for(let i=0; i<numRows2; i++){
-        cleanData2.push(data.rows[i].obj)
-    }
+  barCharts.push(new HorizontalBarChart(barChart02));
 
-
-    let barChart02 = {
-      data: cleanData2,
-      xValue: "Month",
-      yValue: "VALUE",
-      chartWidth: 500,
-      chartHeight: 361,
-      xPos: 900,
-      yPos: 450,
-      axisLineColour: "#d9d9d9",
-      barWidth: 10,
-      labelTextSize: 12,
-      labelPadding: 10,
-      labelColour: "#fc4503",
-      labelRotation: -45,
-      title: "This is a test",
-      xAxisTitle: "This is also a test",
-      xAxisTitle: "Death Cases",
-      yAxisTitle: "Year and Month",
-      title: "Total of Registered Deaths (From 2021-2023)",
-      TitleSize: 12,
-      labelTextSize: 12,
-    };
-
-    barCharts.push(new HorizontalBarChart(barChart02))
-
-
-// stacked bar chart
-
-numRows3 = data3.rows.length;
-for(let i=0; i<numRows3; i++){
-    cleanData3.push(data3.rows[i].obj)
+//  second horizontal bar chart 
+numRows5 = data5.rows.length;
+for (let i = 0; i < numRows5; i++) {
+  cleanData5.push(data5.rows[i].obj);
 }
-    let barChart03 = {
-        data: cleanData3,
-        xValue: "YEAR",
-        yValues: ["MALE", "FEMALE"],
-        chartWidth: 500,
-        chartHeight: 361,
-        xPos: 200,
-        yPos: 1100,
-        axisLineColour: "#d9d9d9",
-        barWidth: 10,
-        labelTextSize: 12,
-        labelPadding: 10,
-        labelColour: "#fc4503",
-        labelRotation: 45,
-        xAxisTitle: "Year",
-        yAxisTitle: "Death Cases",
-        title: "Male VS Female Deaths",
-        TitleSize: 200,
-        colours: ["#6c173e","#02cdb1"]
-      };
-  
-  
-      barCharts.push(new StackedBarChart(barChart03))
+
+let barChart05 = {
+  data: cleanData5,
+  xValue: "Month",
+  yValue: "VALUE",
+  chartWidth: 500,
+  chartHeight: 361,
+  xPos: 200,
+  yPos: 1100,
+  axisLineColour: "#d9d9d9",
+  barWidth: 10,
+  labelTextSize: 12,
+  labelPadding: 10,
+  labelColour: "#fc4503",
+  labelRotation: -45,
+  title: "This is a test",
+  xAxisTitle: "Year and Month",
+  yAxisTitle: "Death Cases",
+  title: "Total of Registered Female Deaths (From 2019-2021)",
+  TitleSize: 12,
+  labelTextSize: 12,
+};
+
+  barCharts.push(new HorizontalBarChart(barChart05));
+
+
+
+  // stacked bar chart
+  numRows3 = data3.rows.length;
+  for (let i = 0; i < numRows3; i++) {
+    cleanData3.push(data3.rows[i].obj);
+  }
+  let barChart03 = {
+    data: cleanData3,
+    xValue: "YEAR",
+    yValues: ["MALE", "FEMALE"],
+    chartWidth: 500,
+    chartHeight: 361,
+    xPos: 200,
+    yPos: 1700,
+    axisLineColour: "#d9d9d9",
+    barWidth: 10,
+    labelTextSize: 12,
+    labelPadding: 10,
+    labelColour: "#fc4503",
+    labelRotation: 45,
+    xAxisTitle: "Year",
+    yAxisTitle: "Death Cases",
+    title: "Male VS Female Deaths",
+    TitleSize: 200,
+    colours: ["#6c173e", "#02cdb1"],
+  };
+
+  barCharts.push(new StackedBarChart(barChart03));
 }
-
-
-
 
 function draw() {
-    background(50);
-    barCharts.forEach(bar => bar.render())
+  background(50);
+  barCharts.forEach((bar) => bar.render());
 }
-
